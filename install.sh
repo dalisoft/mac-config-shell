e1b54ffcdf192da86e10450c8d436dda704996b7
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2021-10-23 05:18:37 +0500

    fix: migrate to latest nodejs v17

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ LINK_FILES=(".nanorc" ".vimrc" ".tmux.conf" ".gitconfig")
 NPM_PACKAGES=("npm" "0x" "cordova" "esy" "flamebearer" "http-server" "node-gyp" "nodemon" "npm-check-updates" "typesync")
 PIP_PACKAGES=("virtualenv" "jupyterlab" "notebook" "labelme" "psrecord")
 
-FNM_VERSIONS=("12.22.7" "14.18.1")
+FNM_VERSIONS=("16.12.0")
 
 #############################
 ### Preparations of steps ###
