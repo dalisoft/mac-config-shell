6a7523ddd32e653c0da6b045b16a72f542ddc5d5
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2023-02-02 19:25:11 +0500

    fix: remove manual casks as these still updating

--- install.sh
+++ install.sh
@@ -376,7 +376,7 @@ function install_system_packages {
 
   # Installing bundle
   brew bundle --no-lock --verbose
-  brew install --cask casks/*.rb
+  #brew install --cask casks/*.rb
 }
 
 ### Installation npm packages
